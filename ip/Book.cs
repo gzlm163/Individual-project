896a810d295eeaeed5107279761ed54874d91b90
gzlm163 <marusyagzl@gmail.com>
2026-05-27 17:54:48 +0700

    Create project and add Book class

---
+++ ip/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Book
+    {
+    public string Name;
+    public int Id;
+    public string Author;
+    public double Price;
+
+    public Book(int id, string name, string author, double price)
+    {
+        Id = id;
+        Name = name;
+        Author = author;
+        Price = price;
+    }
+}
+
