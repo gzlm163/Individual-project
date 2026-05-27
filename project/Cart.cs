2d31f2728e38932229f1219932c29ee89ceef9af
gzlm163 <marusyagzl@gmail.com>
2026-05-27 18:41:09 +0700

    Add Cart class

---
+++ project/Cart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Cart
+{
+    private List<Book> _books = new List<Book>();
+
+    public void Add(Book book)
+    {
+        _books.Add(book);
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index >= 0 && index < _books.Count)
+        {
+            _books.RemoveAt(index);
+        }
+    }
+
+    public string Display()
+    {
+        if (_books.Count == 0)
+        {
+            return "Корзина пуста";
+        }
+
+        string result = "";
+        for (int bookIndex = 0; bookIndex < _books.Count; ++bookIndex)
+        {
+            result = result + $"{bookIndex + 1}. {_books[bookIndex].Name} - {_books[bookIndex].Price} руб.\n";
+        }
+        result = result + $"Итого: {GetTotal()} руб.\n";
+        return result;
+    }
+
+    public double GetTotal()
+    {
+        double total = 0;
+        foreach (Book book in _books)
+        {
+            total += book.Price;
+        }
+        return total;
+    }
+
+    public void Clear()
+    {
+        _books.Clear();
+    }
+}
